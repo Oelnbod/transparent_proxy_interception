3e697387d0eedb13b38f8d268e67b9289b4f4771
Oelnbod <joel.body@proton.me>
2025-02-17 15:00:02 +0000

    Delete install.sh

--- install.sh
+++
@@ -1,4 +0,0 @@
-sudo apt install squid iptables dnsutils -y
-cp squid.conf /etc/apt/squid.conf
-sudo iptables -t nat -A PREROUTING -i $1 -p tcp --dport 80 -j REDIRECT --to-port 3128
-sudo iptables -t nat -A PREROUTING -i $1 -p tcp --dport 443 -j REDIRECT --to-port 3128
